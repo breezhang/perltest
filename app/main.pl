bc890bd85d3ec35a73986ece3e32b920aa2deb32
breezang1877@hotmail.com <breezang1877@hotmail.com>
2014-04-06 23:11:53 +0800

    parse cookie  add https for curl::simple

--- app/main.pl
+++ app/main.pl
@@ -7,21 +7,36 @@ use Data::Dumper::Concise;
 
 use WWW::Curl::Simple;
 
-my $curl = WWW::Curl::Simple->new();
-
-my $res = $curl->get('http://www.163.com/');
+my $curl =
+    WWW::Curl::Simple->new( ssl_cert_bundle =>
+        "D:\\Curl\\curl-7.33.0-win32\\curl-7.33.0-win32\\bin\\curl-ca-bundle.crt"
+    );
+my $res = $curl->get('https://www.debuggex.com/');
 
 if ( $res->is_success ) {
 
     #print $res->decoded_content;
     #$res->decoded_content;
-    say $res->code;
-    say $res->message;
-    say $res->header("Access-Control-Allow-Origin");
-    say "Age ",$res->header("Age");
-    say "Range",$res->header("Accept-Ranges");
-    say $res->header("Content-MD5");
-    say $res->header("Content-MD5");
+    $service = $res->header("Set-Cookie");
+
+    $cookie = {};
+    for ( split /;/, $service ) {
+        if (m{(\w+)=(.+)|(\w+)}x) {
+            if ( !$2 && !$1 ) {
+                $cookie->{$3} = "";
+                next;
+            }
+            if ( !$cookie->{$1} ) {
+                $cookie->{$1} = $2;
+            }
+            else {
+                $cookie->{ $1 . "E" } = $2;
+            }
+        }
+    }
+
+    say Dumper($cookie);
+
 }
 else {
     print STDERR $res->status_line, "\n";
