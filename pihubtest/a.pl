1e7494d1fa501cfc17691b6e8dc27242110e4771
breezang1877@hotmail.com <breezang1877@hotmail.com>
2014-05-07 13:48:02 +0800

    pithub gits basic example more

--- pihubtest/a.pl
+++ pihubtest/a.pl
@@ -1,27 +1,37 @@
 #!
-#just tell syn check not pm file
 use strict;
 use warnings;
 use Pithub;
 use LWP::UserAgent;
 use Data::Dumper::Concise;
 use feature ':5.14';
+use Carp;
 
-#new
-my $token = q{21867e7832cf596f2d68653c5063fd033d2cb4dd};
-
-my $ua = LWP::UserAgent->new(
-    agent =>
-        'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
-    timeout => 180
-);
-
-my $p = Pithub::Gists->new( token => $token, ua => $ua, user => 'breezhang' );
-
-my $result = $p->get( gist_id => '9969380' );
-if ( $result->success ) {
-    #print $result->content->{html_url};
-    my @list = %{$result->content->{files}};
-    print @list;
+sub get_token {
+    my $pt = $ENV{HOME} . '\.gist-vim';
+    open my $FH, '<', $pt || croak;
+    my %tok = split / /, ( grep { chomp; /token\s+[0-9a-g]+/x; } <$FH> )[0];
+    close $FH;
+    return $tok{token};
 }
 
+{
+    my $ua = LWP::UserAgent->new(
+        agent =>
+            'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
+        timeout => 180
+    );
+
+    my $p = Pithub::Gists->new( token => get_token(),
+                                ua    => $ua,
+                                user  => 'breezhang'
+    );
+
+    my $result = $p->get( gist_id => '9969380' );
+    if ( $result->success ) {
+
+        my @list = %{ $result->content->{files} };
+        print @list;
+    }
+
+}
