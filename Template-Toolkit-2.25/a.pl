ba3d27cfb393d6d8ca7b9e4151bea4ce05f005fd
breezang1877@hotmail.com <breezang1877@hotmail.com>
2014-05-13 20:57:44 +0800

    add some demo

--- Template-Toolkit-2.25/a.pl
+++ Template-Toolkit-2.25/a.pl
@@ -7,14 +7,22 @@ use Template;
 use Carp qw /croak carp confess/;
 
 my $config = {
-    INCLUDE_PATH => '/search/path',  # or list ref
+    INCLUDE_PATH => 'Template',  # or list ref
     INTERPOLATE  => 1,               # expand "$var" in plain text
     POST_CHOMP   => 1,               # cleanup whitespace
-    PRE_PROCESS  => 'header',        # prefix each template
+    #PRE_PROCESS  => 'header',        # prefix each template
     EVAL_PERL    => 1,               # evaluate Perl code blocks
+    CACHE_SIZE   =>1024*8,
+    COMPILE_DIR  => "$ENV{APPDATA}\\Local\\Temp",
+    DEBUG =>1
 };
 
+say "$ENV{APPDATA}\\Local\\Temp";
 
-my $tt2 = Template->new(INCLUDE_PATH => 'Template', POST_CHOMP => 1);
+
+my $tt2 = Template->new($config);
 
 $tt2->process('welcome.tt2') || croak $tt2->error(), "\n";
+$tt2->process('goodbye.tt')  || croak $tt2->error(), "\n";
+$tt2->process('wrapper.tt2') || croak $tt2->error(), "\n";
+$tt2->process('example.tt2') || croak $tt2->error(), "\n";
